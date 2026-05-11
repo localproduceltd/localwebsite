2067bf94dad632ac1afc2266a0d7f674e85f6346
Josie Lockwood <josielockwood1@gmail.com>
2026-05-11 17:15:48 +0100

    Add Google Analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
+import Script from "next/script";
 import "leaflet/dist/leaflet.css";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -24,6 +25,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SL2Y5Z5VNJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SL2Y5Z5VNJ');
+          `}
+        </Script>
+      </head>
       <body className={`${raleway.variable} antialiased bg-surface text-foreground`}>
         <ClerkProvider
           appearance={{
